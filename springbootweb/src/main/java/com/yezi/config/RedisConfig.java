4d12da175a8255155a1093a4d996dcb339f95a3f
guoyemou <55838861+guoyemou@users.noreply.github.com>
2023-03-16 15:48:01 +0800

    Update RedisConfig.java

--- springbootweb/src/main/java/com/yezi/config/RedisConfig.java
+++ springbootweb/src/main/java/com/yezi/config/RedisConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
+@Configuration
 public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
